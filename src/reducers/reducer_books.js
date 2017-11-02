a8f0c841bd94d980c7887ff22222d95ad9da8db4
samvarankashyap <samvaran.kashyap@gmail.com>
2017-11-02 16:27:30 -0400

    updated package.json & added containers for redux booklist #4-42

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function() {
+  return [
+    { title: 'Javascript: The good parts'},
+    { title: 'Harry Potter'},
+    { title: 'The dark tower'},
+    { title: 'Eloquent rubt'},
+    
+  ]
+}
